5c9c6a4ccf17c22f7d9b734e9a93b74f2d84a3ed
Olivier Pimi <pimium@gmail.com>
2020-01-01 20:15:23 +0100

    first messages

---
+++ pc/parts/obj-x86_64-linux-gnu/rotenc.d
@@ -0,0 +1,6 @@
+obj-x86_64-linux-gnu/rotenc.o: rotenc.c ../..//simavr/sim/sim_avr.h \
+ ../..//simavr/sim/sim_irq.h ../..//simavr/sim/sim_interrupts.h \
+ ../..//simavr/sim/sim_avr_types.h ../..//simavr/sim/fifo_declare.h \
+ ../..//simavr/sim/sim_cmds.h ../..//simavr/sim/sim_cycle_timers.h \
+ ../..//simavr/sim/sim_io.h ../..//simavr/sim/sim_avr.h \
+ ../..//simavr/sim/sim_regbit.h rotenc.h
---
+++ pc/parts/obj-x86_64-linux-gnu/ssd1306_glut.d
@@ -0,0 +1,2 @@
+obj-x86_64-linux-gnu/ssd1306_glut.o: ssd1306_glut.c ssd1306_glut.h \
+ ssd1306_virt.h ../..//simavr/sim/sim_irq.h
