d857bb6f7e6c6377bc231df6e5430f332304706e
Olivier Pimi <pimium@gmail.com>
2020-01-20 20:45:26 +0100

    schlagtaster

--- avr/ledramp_avr.c
+++ avr/ledramp_avr.c
@@ -1,14 +1,10 @@
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
-//#include <avr/sleep.h>
+#include <avr/sleep.h>
 //
 // for linker, emulator, and programmer's sake
 #include "avr_mcu_section.h"
-
-// #ifndef MCU
-// #define MCU "attiny13"
-// #endif
 
 #ifndef MCU
 #define MCU attiny13
@@ -16,43 +12,99 @@
 
 AVR_MCU(F_CPU, MCU);
 
-volatile uint8_t tickCount;
+#include <stdio.h>
+/* ------------------------------------------------------------------------- */
+static int uart_putchar(char c, FILE *stream) {
+  if (c == '\n')
+    uart_putchar('\r', stream);
+  loop_until_bit_is_set(UCSR0A, UDRE0);
+  UDR0 = c;
+  return 0;
+}
 
-ISR(TIM0_COMPA_vect) // handler for Output Compare 1 overflow interrupt
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+
+volatile uint8_t rotate_counter = 0;
+volatile uint8_t tickCount;
+volatile uint8_t state = 0;
+
+ISR(TIMER2_COMPA_vect) // handler for Output Compare 1 overflow interrupt
 {
+  sei();
   tickCount++;
 }
 
-void tick_init(void) {
-  TCCR0A = (1 << WGM01);
-  // use CLK/8 prescale value, clear timer/counter on compareA match
-  TCCR0B = (1 << CS00) | (0 << CS01) | (1 << CS02);
+ISR(INT0_vect) {
+  if ((PIND & (1 << PD3))) {
+    rotate_counter++;
+  } else {
+    rotate_counter--;
+  }
+  state = 1;
+}
 
-  OCR0A = 200;
-  TIMSK0 |= (1 << OCIE0A);
+void initEncoder(void) {
+  DDRD &= ~((1 << PD2) | (1 << PD3));
+  EIMSK = 1 << INT0;                   // Enable INT0
+  EICRA = (1 << ISC01) | (1 << ISC00); // Trigger INT0 on rising edge
+}
+
+void tick_init() {
+  /*
+          Timer 2 as RTC
+   */
+  // needs to do that before changing the timer registers
+  // ASYNC timer using a 32k crystal
+  ASSR |= (1 << AS2);
+  TCCR2A = (1 << WGM21);
+  // use CLK/8 prescale value, clear timer/counter on compareA match
+  TCCR2B = (1 << CS20) | (1 << CS21);
+  /*   -- MathPad
+                 clock=32768
+                 prescaler=8
+                 hz=64
+                 (clock/prescaler/hz)-1:63 -- */
+  OCR2A = 127;
+  TIMSK2 |= (1 << OCIE2A);
 }
 
 volatile uint8_t pressed = 0;
 
-ISR(PCINT0_vect) { pressed = (PINB & (1 << PB3)) ? 0 : 1; }
+ISR(PCINT1_vect) {
+  if ((PINC & (1 << PC0)) == 1)
+    pressed++;
+  // wouldn't do that on real hardware, but it's a demo...
+  printf("PCINT1_vect %02x\n", PINC);
+}
 
 int main() {
-  DDRB = 0xf7; // all PORT B output
+  DDRB = 0xff; // all PORT B output
+  DDRC = 0;    // make PORT C input
+  // enable pin change interrupt for PORT C pin 0
+  PCMSK1 |= (1 << PCINT8); // C0
+  PCICR |= (1 << PCIE1);
 
-  //  // enable pin change interrupt for PORT C pin 0
-  GIMSK |= (1 << PCIE);
-  PCMSK |= (1 << PCINT3); // C0
+  stdout = &mystdout;
 
   tick_init();
   sei();
 
-  // uint8_t mask = 0;
+  uint8_t mask = 0;
+  for (;;) {
 
-  while (1) {
+    if (state) {
+      state = 0;
+      mask = 0xff;
+    }
 
-    if (pressed)
-      PORTB = 0x07;
-    else
-      PORTB = (1 << (tickCount & 0x03));
+    if (pressed) {
+      mask <<= 1;
+      pressed = 0;
+      if (!mask)
+        mask = 0x1;
+      PORTB = mask;
+    }
+
+    sleep_mode();
   }
 }
